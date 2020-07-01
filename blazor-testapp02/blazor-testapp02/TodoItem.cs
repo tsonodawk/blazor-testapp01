9bd70dbbfaf3e8501509d34014e3b86c09ca3d3f
Toshiro Sonoda <t.sonoda.wk@gmail.com>
2020-07-02 08:58:59 +0900

    コミット

---
+++ blazor-testapp02/blazor-testapp02/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace blazor_testapp02
+
+{
+    public class TodoItem
+    {
+        //public TodoItem()
+        //{
+        //}
+
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+    }
+}
